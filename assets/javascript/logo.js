3d75a8ae46e2de7c4f889f5b99d2395977035990
Mickey S <marimikaels@gmail.com>
2017-04-12 07:27:44 -0400

    added an about page and a button to it, added a footer, added some code comments

--- assets/javascript/logo.js
+++ assets/javascript/logo.js
@@ -1,16 +1,11 @@
-var elem = document.getElementById('draw-shapes');
-var params = { width: 2000, height: 400 };
+var elem = document.getElementById('draw-shapes'); //tell two.js where to put the logo on the html page
+var params = { width: 2000, height: 400 }; //limit space logo can occupy
 var two = new Two(params).appendTo(elem);
 
-// two has convenience methods to create shapes.
-// makeRectangle two.makeRectangle(x, y, width, height);
-// two.makeCircle(x, y, radius);
-// var circle = two.makeCircle(72, 100, 50);
-// var rect = two.makeRectangle(213, 100, 100, 100);
-// var ellipse = two.makeEllipse(250, 200, 50, 100);
+//draw various shapes which make up the letters in the logo
 var triangle = two.makePolygon(100, 100, 100, 3);
 var triangle2 = two.makePolygon(100, 100, 10, 3);
-// var rect1 = two.makeRectangle(250, 100, 25, 160);
+
 var rect2 = two.makeRectangle(300, 40, 120, 22);
 var rect3 = two.makeRectangle(300, 100, 120, 22);
 var rect4 = two.makeRectangle(300, 160, 120, 22);
@@ -23,17 +18,17 @@ var circle2 = two.makeCircle(895, 100, 10);
 var circle3 = two.makeCircle(1080, 100, 80);
 var circle4 = two.makeCircle(1080, 100, 10);
 var rect9 = two.makeRectangle(1220, 100, 40, 165);
-// var rect10 = two.makeRectangle(1250, 167, 100, 20);
+
 var semi1 = two.makeCurve(1300, 85, 1375, 5, 1450, 85, true);
 var line1 = two.makeLine(1300, 90, 1470, 90);
 var semi2 = two.makeCurve(1310, 120, 1395, 200, 1460, 120, true);
 var line2 = two.makeLine(1320, 160, 1470, 80);
-// var line2 = two.makeLine(1300, 160, 1450, 80);
+
 var underline1 = two.makeRectangle(730, 300, 1200, 22);
 var underline2 = two.makeRectangle(80,300, 20, 20);
 var underline3 = two.makeRectangle(1380,300, 20, 20);
 
-
+//group the shapes as letters so they can be moved together
 var Dgroup = two.makeGroup(triangle, triangle2);
 var Egroup = two.makeGroup(rect2, rect3, rect4);
 var Vgroup = two.makeGroup(rect5, rect6);
@@ -42,9 +37,8 @@ var Ogroup1 = two.makeGroup(circle1, circle2);
 var Ogroup2 = two.makeGroup(circle3, circle4);
 var Sgroup = two.makeGroup(semi1, line1, semi2, line2);
 var underlineGroup = two.makeGroup(underline1, underline2, underline3);
-// Egroup.scale = 0.9;
-// Egroup.translation.set(20, 10);
 
+//add color and define opacity of shapes
 Dgroup.fill = '#FF8000';
 triangle.opacity= 1;
 Dgroup.stroke = "orangered";
@@ -104,7 +98,7 @@ underline3.linewidth = 12;
 underline3.stroke = "royalblue";
 
 
-
+//rotating shapes
 triangle.rotation = 9.96;
 triangle2.rotation = 9.96;
 rect5.rotation = 7.29;
@@ -113,9 +107,11 @@ semi1.rotation = 5.8;
 line1.rotation = 5.8;
 semi2.rotation = 5.8;
 
+
+//the rest of the code animates the shapes in such a way that they will spin every time the page is loaded. 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+  // This code is called everytime two.update() is called, so, about every 60 sec.
+ 
 
   if (Dgroup.scale > 0.9999 && two.frameCount < 5) {
     Dgroup.scale = Dgroup.rotation = 0;
@@ -126,8 +122,7 @@ two.bind('update', function(frameCount) {
 }).play(); 
 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+ 
 
   if (Egroup.scale > 0.9999 && two.frameCount < 5) {
     Egroup.scale = Egroup.rotation = 0;
@@ -138,8 +133,7 @@ two.bind('update', function(frameCount) {
 }).play(); 
 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+  
 
   if (Vgroup.scale > 0.9999 && two.frameCount < 5) {
     Vgroup.scale = Vgroup.rotation = 0;
@@ -150,8 +144,7 @@ two.bind('update', function(frameCount) {
 }).play(); 
 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+
 
   if (Tgroup.scale > 0.9999 && two.frameCount < 5) {
     Tgroup.scale = Tgroup.rotation = 0;
@@ -162,8 +155,7 @@ two.bind('update', function(frameCount) {
 }).play(); 
 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+
   if (Ogroup1.scale > 0.9999 && two.frameCount < 5) {
     Ogroup1.scale = Ogroup1.rotation = 0;
   }
@@ -173,8 +165,7 @@ two.bind('update', function(frameCount) {
 }).play(); 
 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+
   if (Ogroup2.scale > 0.9999 && two.frameCount < 5) {
     Ogroup2.scale = Ogroup2.rotation = 0;
   }
@@ -184,8 +175,7 @@ two.bind('update', function(frameCount) {
 }).play(); 
 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+  
 
   if (rect9.scale > 0.9999 && two.frameCount < 5) {
     rect9.scale = rect9.rotation = 0;
@@ -196,8 +186,7 @@ two.bind('update', function(frameCount) {
 }).play(); 
 
 two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
+ 
 
   if (Sgroup.scale > 0.9999 && two.frameCount < 5) {
     Sgroup.scale = Sgroup.rotation = 0;
@@ -207,28 +196,7 @@ two.bind('update', function(frameCount) {
   Sgroup.rotation += t * 4 * Math.PI;
 }).play(); 
 
-// two.bind('update', function(frameCount) {
-//   // This code is called everytime two.update() is called.
-//   // Effectively 60 times per second.
 
-//   if (underlineGroup.scale > 0.9999 && two.frameCount < 5) {
-//     underlineGroup.scale = underlineGroup.rotation = 0;
-//   }
-//   var t = (1 - underlineGroup.scale) * 0.125;
-//   underlineGroup.scale += t;
-//   underlineGroup.rotation += t * 4 * Math.PI;
-// }).play(); 
-
-
-
-// The object returned has many stylable properties:
-// circle.fill = '#FF8000';
-// circle.stroke = 'orangered'; // Accepts all valid css color
-// circle.linewidth = 5;
-
-// rect.fill = 'rgb(0, 200, 255)';
-// rect.opacity = 0.75;
-// rect.noStroke();
 
 // Don't forget to tell two to render everything
 // to the screen
