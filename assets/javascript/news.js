31106048b2030edd5a3407322c69dcdd28be04c4
mwong770 <mwong770@gmail.com>
2017-05-02 00:06:02 -0400

    only allows user to submit if inputted a search value

--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -11,62 +11,67 @@ $(document).ready(function() {
         event.preventDefault();
         $(".results").empty();
         var userQuery = $("#searchTerm").val().trim();
-        $("#searchTerm").val("");
-     
-        //requests query from reddit
-        var apiResponse = $.ajax({
-            url: 'https://www.reddit.com/subreddits/search.json?q=' + userQuery,
-            method: 'GET',
-            dataType: 'json'
-        });
 
-        //displays query results
-        apiResponse.done(function(response) {
+        //only searches if the user inputs a value
+        if (!userQuery == "") {
 
-            //stores items returned
-            var apiData = response.data.children;
+            $("#searchTerm").val("");
+         
+            //requests query from reddit
+            var apiResponse = $.ajax({
+                url: 'https://www.reddit.com/subreddits/search.json?q=' + userQuery,
+                method: 'GET',
+                dataType: 'json'
+            });
 
-            //sets local variables once outside of for loop
-            var resultDiv;
-            var apiTitleH4;
-            var apiDescP;
-            var button;
-            var apiURLBtn;
+            //displays query results
+            apiResponse.done(function(response) {
 
-            //loops thru items returned and displays them
-            if (apiData.length > 0) {
-                for (var i = 0; i < apiData.length; i++) {
+                //stores items returned
+                var apiData = response.data.children;
 
-                    resultDiv = $("<div id='resultDiv'><br/><hr/>");
+                //sets local variables once outside of for loop
+                var resultDiv;
+                var apiTitleH4;
+                var apiDescP;
+                var button;
+                var apiURLBtn;
 
-                    apiTitle = apiData[i].data.title;
-                    apiTitleH4 = $("<h4>" + apiTitle + "</h4>");
-                    apiTitleH4.attr("id", "articleTitle");
-                    $(resultDiv).append(apiTitleH4);
-                        
-                    apiDesc = apiData[i].data.public_description;
-                    apiDescP = $("<p> Description: " + apiDesc + "</p>");
-                    apiDescP.attr("id", "articleDes");
-                    $(resultDiv).append(apiDescP);
+                //loops thru items returned and displays them
+                if (apiData.length > 0) {
+                    for (var i = 0; i < apiData.length; i++) {
 
-                    button = $("<button>Save</button>");
-                    button.addClass("saveBtn");
-                    button.attr("id", apiTitle);
-                    button.attr("data", "reddit");
-                    $(resultDiv).append(button);
+                        resultDiv = $("<div id='resultDiv'><br/><hr/>");
 
-                    apiURL = 'http://reddit.com' + apiData[i].data.url;
-                    apiURLBtn = $("<button><a href='" + apiURL + "'target='none'>View</a></button><br/>");
-                    apiURLBtn.attr("id", "articleLink");
-                    $(resultDiv).append(apiURLBtn);
-                        
-                    $(".results").append(resultDiv);
-                };
-            }   else {
-                    $(".results").append("<p id='noResultsMessage'> Sorry, no results are available for that search.<br/>Please try another search term.</p>");
-                }
+                        apiTitle = apiData[i].data.title;
+                        apiTitleH4 = $("<h4>" + apiTitle + "</h4>");
+                        apiTitleH4.attr("id", "articleTitle");
+                        $(resultDiv).append(apiTitleH4);
+                            
+                        apiDesc = apiData[i].data.public_description;
+                        apiDescP = $("<p> Description: " + apiDesc + "</p>");
+                        apiDescP.attr("id", "articleDes");
+                        $(resultDiv).append(apiDescP);
 
-        });//ends apiResponse.done
+                        button = $("<button>Save</button>");
+                        button.addClass("saveBtn");
+                        button.attr("id", apiTitle);
+                        button.attr("data", "reddit");
+                        $(resultDiv).append(button);
+
+                        apiURL = 'http://reddit.com' + apiData[i].data.url;
+                        apiURLBtn = $("<button><a href='" + apiURL + "'target='none'>View</a></button><br/>");
+                        apiURLBtn.attr("id", "articleLink");
+                        $(resultDiv).append(apiURLBtn);
+                            
+                        $(".results").append(resultDiv);
+                    };
+                }   else {
+                        $(".results").append("<p id='noResultsMessage'> Sorry, no results are available for that search.<br/>Please try another search term.</p>");
+                    }
+
+            });//ends apiResponse.done
+        }//ends if (!userQuery == "")
 
     });//ends submit click
 
